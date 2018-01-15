9fdf5d6cea3553e0a158b1a4a8b5ea1d82e0d4e0
Michael Crump <mbcrump29@gmail.com>
2018-01-14 17:40:20 -0800

    Add project files.

---
+++ StorageAccountAzure/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageAccountAzure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnection"));
+            var myClient = storageAccount.CreateCloudBlobClient();
+            var container = myClient.GetContainerReference("images-backup");
+            container.CreateIfNotExists(BlobContainerPublicAccessType.Blob);
+
+            var blockBlob = container.GetBlockBlobReference("backup/mikepic.png");
+            using (var fileStream = System.IO.File.OpenRead(@"c:\mikepic.png"))
+            {
+                blockBlob.UploadFromStream(fileStream);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
